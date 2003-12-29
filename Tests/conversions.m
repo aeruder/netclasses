659e36f4a099d18e1331e1a5cb8d736130dac2ab
Andrew Ruder <andy@aeruder.net>
2003-12-29 23:12:40 +0000

    Adding conversions
    git-svn-id: file:///Users/andy/svnstuff/cvsroot/netclasses-svn/trunk@57 fd6dfec2-38a0-4f50-b526-75a5c7907613

---
+++ Tests/conversions.m
@@ -0,0 +1,25 @@
+/***************************************************************************
+                                conversions.m
+                          -------------------
+    begin                : Sun Dec 21 01:37:22 CST 2003
+    copyright            : (C) 2003 by Andy Ruder
+    email                : aeruder@yahoo.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#import <netclasses/NetTCP.h>
+#import <netclasses/NetBase.h>
+
+int main(void)
+{
+	printf("Please enter an ip address: ");
+
+}
